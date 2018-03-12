a58abdf76c77bffe01efbbbff168a5183d68ca34
stonehye <stonehye@gmail.com>
2018-03-12 13:07:18 +0900

    Add files via upload
    3월 9일 과제 (DS18B20에서 온도값을 읽어서 ThingSpeak의 본인 계정의 채널로 1분 주기로 온도 데이타를 전송.  )

---
+++ ds18b20.ino
@@ -0,0 +1,116 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <ESP8266WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS D3
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+const char* ssid     = "MMlab1001";
+const char* password = "mpeglab10";
+
+const char* host = "api.thingspeak.com";
+const char* streamId   = "....................";
+const char* privateKey = "DBJCC280IROXO0NV";
+
+void setup() {
+  Serial.begin(115200);
+  sensors.begin();
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  //WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print("WiFi not connected");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+int value = 0;
+
+void loop() {
+  delay(60000);
+  ++value;
+
+  sensors.requestTemperatures();
+  float strTemp=sensors.getTempCByIndex(0);
+  
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  /*String url = "/input/";
+  url += streamId;
+  url += "?private_key=";
+  url += privateKey;
+  url += "&value=";
+  url += value;*/
+
+  String getStr = "GET /update?api_key=";
+  getStr += privateKey;
+  getStr += "&field1=";
+  getStr += String(strTemp);
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(getStr);
+  
+  // This will send the request to the server
+  /*client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");*/
+
+  client.print(getStr + " HTTP/1.1\r\n" +
+              "Host: " + host + "\r\n" + 
+              "Connection: close\r\n\r\n");
+
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 60000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+}
+
