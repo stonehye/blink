44db3df419b74a81056eb16e2e3d60d904912f9f
stonehye <stonehye@gmail.com>
2018-03-19 01:37:53 +0900

    Add files via upload
    3/19

--- ds18b20.ino
+++ ds18b20.ino
@@ -1,11 +1,6 @@
-/*
- *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
- *
- *  You need to get streamId and privateKey at data.sparkfun.com and paste them
- *  below. Or just customize this script to talk to other HTTP servers.
- *
- */
+#define choose 2
 
+#if (choose == 1)
 #include <ESP8266WiFi.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -113,4 +108,100 @@ void loop() {
   Serial.println();
   Serial.println("closing connection");
 }
+#endif
 
+#if (choose == 2)
+#include <ESP8266WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS D3
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+const char* ssid     = "thejn";
+const char* password = "01040148730";
+
+const char* host = "maker.ifttt.com";
+const char* streamId   = "....................";
+const char* privateKey = "dCzJ3Ne4fknV7-1XMYEDtj";
+
+void setup() {
+  Serial.begin(115200);
+  sensors.begin();
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  //WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print("WiFi not connected");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+int value = 0;
+
+void loop() {
+  delay(5000);
+  ++value;
+
+  sensors.requestTemperatures();
+  float strTemp=sensors.getTempCByIndex(0);
+  
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  String getStr = "POST /trigger/receive_temperature_information/with/key/";
+  getStr += privateKey;
+  getStr += "/?&value1=";
+  getStr += String(strTemp);
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(getStr);
+  
+  client.print(getStr + " HTTP/1.1\r\n" +
+              "Host: " + host + "\r\n" + 
+              "Connection: close\r\n\r\n");
+
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+}
+#endif
